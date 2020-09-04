183a88de6fb9e69321692c0107382b2c40ac38f4
hpw7 <hpw7@student.le.ac.uk>
2020-09-04 12:00:07 +0100

    Metadata decoded earlier in the get_track_title function, allowing for regex to be passed in the function call and used as a plain string

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import stream_handling
 
 def main():
-    print(stream_handling.get_track_title("http://live-absolute.sharp-stream.com/absoluteradio.mp3", "latin1"))
+    print(stream_handling.get_track_title("http://live-absolute.sharp-stream.com/absoluteradio.mp3", "latin1", "StreamTitle='(.*)';S"))
 
 if __name__ == '__main__':
     main()
--- stream_handling.py
+++ stream_handling.py
@@ -7,14 +7,15 @@ except ImportError:
     import urllib.request as urllib2
 
 
-def get_track_title(url, encoding):
+def get_track_title(url, encoding, regex):
     """
     Checks a stream url and returns the currently reported playing song,
     if one is found. Based heavily on https://stackoverflow.com/a/35104372.
 
-    :param url: the URL of the stream to check (mp3, ogg)
+    :param url: the URL string of the stream to check (mp3, ogg)
     :param encoding: latin1, iso-8859-1 for mp3, utf-8 for ogg
-    :return: the reported track title, or "No title found"
+    :param regex: Regular expression string to search for title
+    :return: the reported track title as a string, or "No title found" string
     """
     request = urllib2.Request(url, headers={'Icy-MetaData': 1})  # Requesting metadata
     response = urllib2.urlopen(request)
@@ -22,13 +23,12 @@ def get_track_title(url, encoding):
     for _ in range(5):
         response.read(metaint)  # Skipping past garbage data
         metadata_length = struct.unpack('B', response.read(1))[0] * 16
-        metadata = response.read(metadata_length).rstrip(b'\0')
-        # print(metadata)
-        regex_find = re.search(br"StreamTitle='(.*)';S", metadata)  # Finding title
+        metadata = response.read(metadata_length).rstrip(b'\0').decode(encoding, errors='replace')
+        regex_find = re.search(regex, metadata)  # Finding title
         if regex_find:
             title = regex_find.group(1)
             if title:
                 break
     else:
         return "No title found"
-    return title.decode(encoding, errors='replace')
+    return title
