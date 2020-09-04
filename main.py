9823c55742d4865646eefa76cf84a9bb8b3aff16
hpw7 <hpw7@student.le.ac.uk>
2020-09-04 11:42:56 +0100

    Created basic working function to get the track title from a stream (based heavily on https://stackoverflow.com/a/35104372)

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import stream_handling
+
+def main():
+    print(stream_handling.get_track_title("http://live-absolute.sharp-stream.com/absoluteradio.mp3", "latin1"))
 
 if __name__ == '__main__':
-    print("main.py")
+    main()
