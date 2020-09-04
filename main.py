2c8438f9d9155afa458556a4d03e337e59642590
hpw7 <hpw7@student.le.ac.uk>
2020-09-04 14:26:36 +0100

    A specified text file can be read and then Station objects created from the text data, which are kept in the stream_catalogue for later use.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import stream_handling
+import station
 
 
 def main():
     print(stream_handling.get_track_title("http://live-absolute.sharp-stream.com/absoluteradio.mp3", "latin1",
                                           "StreamTitle='(.*)';S"))
+    stream_catalogue = []
+    streams_txt_location = "streams.txt"
+    streams_txt_separator = "@@@"
+    streams_txt = open(streams_txt_location, "r")
+    streams_count = 0
+    for line in streams_txt:
+        # print(line)
+        line_values = line.strip().split(streams_txt_separator)
+        # print(line_values)
+        station_obj = station.Station.build_from_list(line_values)
+        stream_catalogue.append(station_obj)
 
+    for stream in stream_catalogue:
+        print(stream.name)
 
 if __name__ == '__main__':
     main()
--- station.py
+++ station.py
@@ -8,3 +8,8 @@ class Station:
         self.order = order
         self.playlist_id = playlist_id
         self.include_remixes = include_remixes
+
+    @classmethod
+    def build_from_list(cls, v_v):
+        obj = cls(v_v[0], v_v[1], v_v[2], v_v[3], v_v[4], v_v[5], v_v[6], v_v[7])
+        return obj
