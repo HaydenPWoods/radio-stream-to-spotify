0feda0d9234c80e0cae5b86a35fef7e552af0c9a
hpw7 <hpw7@student.le.ac.uk>
2020-09-20 17:57:37 +0100

    Created list of exclusions in track title and blacklisted artists

--- spotify/track_search.py
+++ spotify/track_search.py
@@ -8,6 +8,12 @@ def track_search(track_title):
     :return: Spotify URI as a String
     """
     tracks, = spotify.search(track_title, types=('track',), limit=1)
+    exclusions = ['karaoke', 'the style of', 'tribute', 'originally performed by', 'includes hidden track',
+                  'bluegrass rendition']
+    blacklisted_artists = ['karaoke', "Pickin' On Series", 'Midifine Systems', 'Studio Allstars',
+                           'Grandes Canciones - Versiones Acústicas', 'Lucky Voice Karaoke', 'The Karaoke Channel',
+                           'Ameritz', 'Poptastik Karaoke', "Singer's Edge Karaoke", 'Brazillian Bossa Nova',
+                           'Nursery Rhymes 123']
     # Likely filter search here to remove obvious / unlikely tracks.
     if tracks.total > 0:
         return tracks.items[0].uri
