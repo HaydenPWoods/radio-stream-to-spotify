3e9c5262bb5b981cbaf5a774cdda92932279e5ff
hpw7 <hpw7@student.le.ac.uk>
2020-09-24 14:13:33 +0100

    More intelligent track searching implemented, filtering out bad tracks (karaoke, covers)

--- spotify/track_search.py
+++ spotify/track_search.py
@@ -4,16 +4,46 @@ from spotify import spotify
 def track_search(track_title):
     """
     Takes a track title as a string, searches for the song on Spotify, and then returns the URI of the song if found.
+    Attempts to exclude karaoke and 'cover tracks' from spam artists.
     :param track_title: String
-    :return: Spotify URI as a String
+    :return: Spotify URI as a String, or NoneType if no suitable track found.
     """
-    tracks, = spotify.search(track_title, types=('track',), limit=1)
+    tracks, = spotify.search(track_title, types=('track',), limit=5)
     exclusions = ['karaoke', 'the style of', 'tribute', 'originally performed by', 'includes hidden track',
                   'bluegrass rendition']
-    blacklisted_artists = ['karaoke', "Pickin' On Series", 'Midifine Systems', 'Studio Allstars',
+    blacklisted_artists = ['Karaoke', "Pickin' On Series", 'Midifine Systems', 'Studio Allstars',
                            'Grandes Canciones - Versiones Acústicas', 'Lucky Voice Karaoke', 'The Karaoke Channel',
                            'Ameritz', 'Poptastik Karaoke', "Singer's Edge Karaoke", 'Brazillian Bossa Nova',
                            'Nursery Rhymes 123']
+    good_track = -1
     # Likely filter search here to remove obvious / unlikely tracks.
     if tracks.total > 0:
-        return tracks.items[0].uri
+        print(tracks.total)
+        if tracks.total < 5:
+            tracks_search_num = tracks.total
+        else:
+            tracks_search_num = 5
+        for i in range(tracks_search_num):
+            curr_track = tracks.items[i]
+            bad_track = False
+            for entry in exclusions:
+                if entry in curr_track.name:
+                    bad_track = True
+                    break
+            if bad_track:
+                continue
+            else:
+                for bad_artist in blacklisted_artists:
+                    for artist in curr_track.artists:
+                        if bad_artist in artist.name:
+                            bad_track = True
+                            break
+                    if bad_track:
+                        break
+                if bad_track:
+                    continue
+                else:
+                    good_track = curr_track
+                    break
+        if good_track != -1:
+            return good_track
