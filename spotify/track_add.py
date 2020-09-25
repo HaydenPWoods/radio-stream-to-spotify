76733b6753ec13fef332b58fed7680918030856a
hpw7 <hpw7@student.le.ac.uk>
2020-09-25 14:35:10 +0100

    track_add now removes any old duplicates of the current track, and limits playlist size

--- spotify/track_add.py
+++ spotify/track_add.py
@@ -7,4 +7,18 @@ def track_add(spotify_track, playlist_id):
     :param spotify_track: FullTrack
     :param playlist_id: String
     """
-    spotify.playlist_add(playlist_id, [spotify_track.uri], 0)  # Adding the track to the playlist
+    tries = 0
+    while tries <= 5:
+        try:
+            max_tracks = 50  # Maximum number of tracks in the playlist
+            spotify_playlist = spotify.playlist(playlist_id)
+            if (spotify_playlist.tracks.total >= max_tracks):
+                # Number of tracks in playlist has reached the threshold, so remove oldest.
+                spotify.playlist_remove_indices(playlist_id, [spotify_playlist.tracks.total - 1], spotify_playlist.snapshot_id)
+            spotify.playlist_remove(playlist_id, [spotify_track.uri])
+            spotify.playlist_add(playlist_id, [spotify_track.uri], 0)  # Adding the track to the playlist
+            break
+        except Exception as e:
+            print("Something went wrong, but retrying...")
+            print(e)
+            tries += 1
