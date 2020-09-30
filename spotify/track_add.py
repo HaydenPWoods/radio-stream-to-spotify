1854d46764eec19097bc43f91bd5a9a86872a51d
hpw7 <hpw7@student.le.ac.uk>
2020-09-30 12:06:21 +0100

    Checking size of playlist after adding current track

--- spotify/track_add.py
+++ spotify/track_add.py
@@ -12,13 +12,13 @@ def track_add(spotify_track, playlist_id):
     while tries <= 5:
         try:
             max_tracks = 100  # Maximum number of tracks in the playlist
+            spotify.playlist_remove(playlist_id, [spotify_track.uri])
+            spotify.playlist_add(playlist_id, [spotify_track.uri], 0)  # Adding the track to the playlist
             spotify_playlist = spotify.playlist(playlist_id)
             if spotify_playlist.tracks.total > max_tracks:
                 # Number of tracks in playlist has reached the threshold, so remove oldest.
                 spotify.playlist_remove_indices(playlist_id, [spotify_playlist.tracks.total - 1],
                                                 spotify_playlist.snapshot_id)
-            spotify.playlist_remove(playlist_id, [spotify_track.uri])
-            spotify.playlist_add(playlist_id, [spotify_track.uri], 0)  # Adding the track to the playlist
             break
         except Exception as e:
             print("Something went wrong, but retrying...")
