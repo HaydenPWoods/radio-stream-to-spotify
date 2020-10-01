718d468f7eb30bde24920d183f81ddba0bdf43dd
hpw7 <hpw7@student.le.ac.uk>
2020-10-01 15:49:05 +0100

    Updating spotify track search limit back to 5 because of poor experience

--- spotify/track_search.py
+++ spotify/track_search.py
@@ -12,14 +12,14 @@ def track_search(track_title, artist_name, include_remixes):
     :return: Spotify URI as a String, or NoneType if no suitable track found.
     """
     try:
-        tracks, = spotify.search(track_title, types=('track',), limit=10)
+        tracks, = spotify.search(track_title, types=('track',), limit=5)
         good_track = -1
         if tracks.total > 0:
             print(tracks.total)
-            if tracks.total < 10:
+            if tracks.total < 5:
                 tracks_search_num = tracks.total
             else:
-                tracks_search_num = 10
+                tracks_search_num = 5
             for i in range(tracks_search_num):
                 curr_track = tracks.items[i]
                 bad_track = False
