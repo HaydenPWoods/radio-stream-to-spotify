167a3fccba9ca78a9cadf9c2da1a4a29d26ff5ed
hpw7 <hpw7@student.le.ac.uk>
2020-10-01 12:48:26 +0100

    Cleaning up main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import stream_handling
 from station import Station
-from spotify.track_search import track_search, track_search_legacy
+from spotify.track_search import track_search
 from spotify.track_add import track_add
 import time
 
 
 def main():
-    # stream_track = stream_handling.get_track_title("http://live-absolute.sharp-stream.com/absoluteradio.mp3", "latin1",
-    #                                               "StreamTitle='(.*)';S")
-    # print(stream_track)
-    # spotify_track = track_search(stream_track)
-    # track_add(spotify_track, "1viwaS9PU13aFMTqH1KB64")
     stream_catalogue = []
     streams_txt_location = "streams.txt"
     streams_txt_separator = "@@@"
     streams_txt = open(streams_txt_location, "r")
     for line in streams_txt:
-        print(line)
         line_values = line.strip().split(streams_txt_separator)
         print(line_values)
         station_obj = Station.build_from_list(line_values)
@@ -31,10 +25,10 @@ def main():
                     stream_track_split = stream_track.strip().split(stream.separator)
                     if int(stream.order) == 1:
                         spotify_track = track_search(stream_track_split[0].strip(), stream_track_split[1].split()[0],
-                                                    stream.include_remixes)
+                                                     stream.include_remixes)
                     else:
                         spotify_track = track_search(stream_track_split[1].strip(), stream_track_split[0].split()[0],
-                                                    stream.include_remixes)
+                                                     stream.include_remixes)
                     if spotify_track is not None:
                         track_add(spotify_track, stream.playlist_id)
             except Exception as e:
