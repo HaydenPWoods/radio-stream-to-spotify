612153bc3fbc15d81f46f4fb81075ff9290b486b
Hayden Woods <44874805+HaydenPWoods@users.noreply.github.com>
2021-08-28 13:25:18 +0100

    Old Virgin Radio API no longer available, updating virgin_handling for new method

--- virgin_handling.py
+++ virgin_handling.py
@@ -4,6 +4,24 @@ import requests
 
 
 def get_track_title(url):
+    """
+    Given a Virgin Radio UK API URL, gets the latest played song for the given station and formats this into a "Artist -
+    Title" string.
+
+    :param url: The API request link. The parameters "station", "withSongs", "hasPrograms", and "numberOfSongs" are all
+                required.
+    :return: The most recently played song, if available, formatted as an "artist - title" string.
+    """
+    try:
+        virgin_json = json.loads(requests.get(url).text)
+        return virgin_json["recentlyPlayed"][0]["artist"] + " - " + virgin_json["recentlyPlayed"][0]["title"]
+    except Exception as e:
+        print("Some exception occurred... (virgin_handling)")
+        print(e)
+        return "No title found"
+
+
+def get_track_title_legacy(url):
     try:
         virgin_json = json.loads(requests.get(url).text
                                  .replace("jsonCallback_virgin(", "")
